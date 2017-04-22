18fa7e4a526b773847a75b381c319488a15ce85b
jesse <jsoldat@hotmail.com>
2017-04-22 07:56:00 +0700

    delete test works

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-process.env.MONGODB_URI = "mongodb://jesse:jesse@ds111771.mlab.com:11771/udemy-node"
+// process.env.MONGODB_URI = "mongodb://jesse:jesse@ds111771.mlab.com:11771/udemy-node"
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
